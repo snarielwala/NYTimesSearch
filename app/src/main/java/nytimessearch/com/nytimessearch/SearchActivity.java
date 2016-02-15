b3e3348974886da2a9ea121cf578fdc566ff9de2
Samarth <samarth.narielwala@gmail.com>
2016-02-14 20:14:27 -0500

    All Basic Stuff Done

--- app/src/main/java/nytimessearch/com/nytimessearch/SearchActivity.java
+++ app/src/main/java/nytimessearch/com/nytimessearch/SearchActivity.java
@@ -1,29 +1,97 @@
 package nytimessearch.com.nytimessearch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    Button btnSearch;
+    GridView gvResults;
+    ArrayList<Article> articles;
+    ArticleArrayAdapter adapter;
+    String date = "";
+    String sortOrder = "";
+    boolean isArts = false;
+    boolean isFashion=false;
+    boolean isSports=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,5);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        GridView gridView = (GridView) findViewById(R.id.gvResults);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(toolbar);
+        setupViews();
+
+        gridView.setOnScrollListener(new EndlessScrollListener(gridView) {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to your AdapterView
+                addMoreResults(page);
+                // or customLoadMoreDataFromApi(totalItemsCount);
+                return true; // ONLY if more data is actually being loaded; false otherwise.
+            }
+        });
+    }
+
+    public void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        articles = new ArrayList<Article>();
+        adapter = new ArticleArrayAdapter(this, articles);
+        gvResults.setAdapter(adapter);
+
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //create an intent to display the article
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+
+                //get the article to display
+                Article article = articles.get(position);
+                i.putExtra("article", article);
+
+                startActivity(i);
+                //pass in that article into intent
+
+                //launch the activity
             }
         });
     }
@@ -34,6 +102,7 @@ public class SearchActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,4 +118,79 @@ public class SearchActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onArticleSearch(View view) {
+        adapter.clear();
+        addMoreResults(0);
+    }
+
+    public void addMoreResults(int page){
+        if(!isNetworkAvailable()) {
+            Toast.makeText(this,"No Network Connectivity, Try Again !",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String query = etQuery.getText().toString();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("api-key", "ffcb95b28db4e3f1dd90bef0879667a3:17:74338942");
+        requestParams.put("page", page);
+        requestParams.put("q", query);
+        if(!date.isEmpty())requestParams.put("begin_date",date);
+        if(!sortOrder.isEmpty())requestParams.put("sort",sortOrder);
+
+        client.get(url, requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+
+                JSONArray articleJsonResults = null;
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    Log.d("DEBUG", articleJsonResults.toString());
+                    articles.addAll(Article.fromJsonArray(articleJsonResults));
+                    adapter.notifyDataSetChanged();
+                    Log.d("DEBUG", articles.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+
+    public void addFilters(MenuItem item) {
+        Intent i = new Intent(getApplicationContext(), FilterActivity.class);
+        startActivityForResult(i, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Filter Preferences Saved", Toast.LENGTH_LONG).show();
+
+            this.date = data.getStringExtra("date").replace("/","");
+            this.sortOrder=data.getStringExtra("sort");
+            this.isArts=data.getBooleanExtra("isArts",false);
+            this.isFashion=data.getBooleanExtra("isFashion",false);
+            this.isSports=data.getBooleanExtra("isSports",false);
+        }
+    }
+
+
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
 }
+
